b7ad8c8c354dbba5ac7221bcfacf5f87f865c20d
Filipe <filipeccm@gmail.com>
2020-08-31 13:18:13 +0200

    deleted remaining code from redux-persist, which was deleted

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,16 +1,8 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import beersReducer from './beers/beers.reducer';
 import favoriteBeersReducer from './favorite-beers/favorite-beers.reducer';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['beers'],
-};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -18,4 +10,4 @@ const rootReducer = combineReducers({
   favoriteBeers: favoriteBeersReducer,
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
